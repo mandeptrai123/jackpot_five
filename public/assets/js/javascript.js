1b09af8d8fd9589eb6141d01b2635f582e8cb386
mandeveloper2k <53419763+mandeptrai123@users.noreply.github.com>
2021-02-06 11:18:16 +0700

    .

--- public/assets/js/javascript.js
+++ public/assets/js/javascript.js
@@ -112,6 +112,7 @@ Gframe.compatibility = {
 	'audio': false, // to get the primitive value result, use "Boolean(Gframe.compatibility.audio)"
 	'cssanimations': false,
 	'touch': false
+
 };
 
 /*
@@ -690,15 +691,16 @@ $(function() {
 	var _currentStep = 0;
 
 	$('#spin_reels').click(function(){
+
 		Backbone.mediator.trigger( 'spinTheReel' );
-		return false;
+		//return false;
 		var _sound_num = Gframe.preloadManifest.length - 1;
 		if(_soundTest_index > _sound_num) _soundTest_index = 0;
 		Gframe.memory.mov({
 			id: 'sound_code',
 			value: Gframe.preloadManifest[_soundTest_index].id});
 		Backbone.mediator.trigger( 'stopSound' );
-		Backbone.mediator.trigger( 'playSound' , Gframe.preloadManifest[_soundTest_index].id );
+		//Backbone.mediator.trigger( 'playSound' , Gframe.preloadManifest[_soundTest_index].id );
 		_soundTest_index = _soundTest_index+ 1;
 	});
 
@@ -710,25 +712,29 @@ $(function() {
 		Backbone.mediator.trigger( 'betControl', false);
 	});
 
+	$('#bet_two').click(function(){
+		Backbone.mediator.trigger( 'betControl', true);
+	});
+
 
 	$(document).keydown(function(_event){
 		var _keyCode = _event.keyCode;
 		switch(_keyCode) {
 			case 32: // space
-				Backbone.mediator.trigger( 'spinTheReel');
+				Backbone.mediator.trigger( 'spinTheReel',3);
 				break;
 			case 68: // d
-				//var _doorTransparency = ($('.marquee, .frontface-plate').css('opacity') == 1) ? 0.5 : 1;
-				//$('.marquee, .frontface-plate').css('opacity',_doorTransparency);
+				// var _doorTransparency = ($('.marquee, .frontface-plate').css('opacity') == 1) ? 0.5 : 1;
+				// $('.marquee, .frontface-plate').css('opacity',_doorTransparency);
 				break;
 			case 49: // 1
-				//Gframe.reels.spinControl (1)
+				Gframe.reels.spinControl (1);
 				break;
 			case 50: // 2
-				//Gframe.reels.spinControl (2)
+				Gframe.reels.spinControl (2);
 				break;
 			case 51: // 3
-				//Gframe.reels.spinControl (3)
+				Gframe.reels.spinControl (3);
 				break;
 			case 52: // 4
 				// _currentStep = (_currentStep > 22) ? 0 : _currentStep + 1 ;
@@ -743,13 +749,13 @@ $(function() {
 				// Backbone.mediator.trigger( 'reelStopper', 3 );
 				break;
 			case 83: // s
-				//Backbone.mediator.trigger( 'spinTheReel', 1 );
+				Backbone.mediator.trigger( 'spinTheReel', 3);
 				break;
 			case 66: // b
-				//Gframe.vfx.showVFX('coins_smallWin');
+				Gframe.vfx.showVFX('coins_smallWin');
 				break;
 			case 67: // c
-				//Gframe.vfx.eraseVFX();
+				Gframe.vfx.eraseVFX();
 				break;
 			case 80: // p (price decal)
 				var $_price_decal = $('#price_decal');
@@ -1409,10 +1415,12 @@ Gframe.backbone.views.reels = Backbone.View.extend({
 
 		Backbone.mediator.trigger( 'playSound' , this.currentReelSound );
 	},
+	
 	spinControl: function (_id) {
 		var _currentReelID = 'reel-'+_id;
 		var $_reel = $('#'+_currentReelID);
 		var _spin = $_reel.hasClass('spin');
+	
 		if(!_spin) {
 			this.active_reels[_currentReelID] = true;
 		} else {
